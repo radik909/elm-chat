5d05dd9df8bd221391a27c3364367f680f5b4791
RADIK <rahuldinesh1991@gmail.com>
2016-11-04 17:27:55 +0530

    Basic implementation of a form

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,22 +1,14 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.App as App
 import Html.Attributes exposing (..)
-import Html.App as Html
-import Html.Events exposing (onClick)
-
-
--- component import example
-
-import Components.Hello exposing (hello)
-
-
--- APP
+import Html.Events exposing (onInput)
 
 
 main : Program Never
 main =
-    Html.beginnerProgram { model = model, view = view, update = update }
+    App.beginnerProgram { model = model, view = view, update = update }
 
 
 
@@ -24,12 +16,15 @@ main =
 
 
 type alias Model =
-    Int
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
 
 
-model : number
+model : Model
 model =
-    0
+    Model "" "" ""
 
 
 
@@ -37,58 +32,45 @@ model =
 
 
 type Msg
-    = NoOp
-    | Increment
+    = Name String
+    | Password String
+    | PasswordAgain String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        NoOp ->
-            model
+        Name name ->
+            { model | name = name }
 
-        Increment ->
-            model + 1
+        Password password ->
+            { model | password = password }
+
+        PasswordAgain password ->
+            { model | passwordAgain = password }
 
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
-        [ -- inline CSS (literal)
-          div [ class "row" ]
-            [ div [ class "col-xs-12" ]
-                [ div [ class "jumbotron" ]
-                    [ img [ src "static/img/elm.jpg", style styles.img ] []
-                      -- inline CSS (via var)
-                    , hello model
-                      -- ext 'hello' component (takes 'model' as arg)
-                    , p [] [ text ("Elm Webpack Starter") ]
-                    , button [ class "btn btn-primary btn-lg", onClick Increment ]
-                        [ -- click handler
-                          span [ class "glyphicon glyphicon-star" ] []
-                          -- glyphicon
-                        , span [] [ text "FTW!" ]
-                        ]
-                    ]
-                ]
-            ]
+    div []
+        [ input [ type' "text", placeholder "Name", onInput Name ] []
+        , input [ type' "password", placeholder "Password", onInput Password ] []
+        , input [ type' "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , viewValidation model
         ]
 
 
-
--- CSS STYLES
-
-
-styles : { img : List ( String, String ) }
-styles =
-    { img =
-        [ ( "width", "33%" )
-        , ( "border", "4px solid #337AB7" )
-        ]
-    }
+viewValidation : Model -> Html msg
+viewValidation model =
+    let
+        ( color, message ) =
+            if model.password == model.passwordAgain then
+                ( "green", "OK" )
+            else
+                ( "red", "Passwords do not match!" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
